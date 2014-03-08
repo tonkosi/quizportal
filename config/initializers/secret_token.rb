21897df3d7c9f325131161fa8400947fe3cf4579
Mihael Liskij <mihael.liskij@gmail.com>
2014-03-08 23:22:12 +0100

    Installed the ember framework in rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quizportal::Application.config.secret_key_base = 'b851806c8d733229a3d01781ab0f6ae4df996464976d2c4c06fa31824fb5069c8e3b6c9fc347f976bc5249a263d9e0bf3dabbc2697a67a7506198a7f48280e43'
