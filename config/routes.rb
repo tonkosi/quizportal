f7fe2185e5d3ff240ddee89d560d5cbf61de7f2f
Mihael Liskij <mihael.liskij@gmail.com>
2014-06-17 23:12:59 +0200

    Connected rails routes to ember.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Quizportal::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get '*foo' => 'ember#index'
+  root 'ember#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
