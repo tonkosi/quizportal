d0d682b4fd8bcfaed465dc06e1e10f08c745586e
Mihael Liskij <mihael.liskij@gmail.com>
2014-07-20 14:40:01 +0200

    Added routes for the ember app in rails and added routes in ember for the index and about page template

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'ember#index'
+  root :to => 'ember#index'
+  match "/*path" => 'ember#index', via: :all
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
