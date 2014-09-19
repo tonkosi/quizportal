7e06e7b8f7b306956fec2081f884f7b798627de9
Mihael Liskij <mihael.liskij@gmail.com>
2014-09-19 15:56:16 +0200

    Added groups to api

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   scope '/api' do
     resources :users
+    resources :groups
     resources :sessions, only: [ :index, :create ]
     match "/sessions" => 'sessions#destroy', via: :delete
   end
